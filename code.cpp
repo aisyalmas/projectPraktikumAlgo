69f5ac0ec7131d07ced16035aff548e2c77a11de
aisyalmas <almasaisyah06@gmail.com>
2026-05-18 13:41:41 +0700

    login part

--- code.cpp
+++ code.cpp
@@ -4,15 +4,16 @@ using namespace std;
 int isexit = false;
 
 struct users{
-    string name;
+    string username;
     string password;
 };
 
 
 
 int main(){
-    int jumlahUser=100,menuAwal;
-    users user[jumlahUser];
+    int jumlahUser=0,menuAwal,userMax=100;
+    string usernameNew,passwordNew,logPass,logUser;
+    users user[userMax];
 
     do{
         
@@ -36,15 +37,64 @@ int main(){
         }while (menuAwal<1 || menuAwal>3);
     
         switch (menuAwal){
-        case 1:
+        case 1:{
+
             //register
+            //1. pengkondisian apakah memori masi ada
+            if(jumlahUser>= userMax){
+                cout<<" Memori penuh! tidak bisa daftar";
+                break;
+            }
+            //2. masukin uname
+            bool found=false;
+            do{
+                cout<<"\n=========== REGISTER ============";
+                cout<<"\n Username: ";cin>>usernameNew;
+                for(int i;i<jumlahUser;i++){
+
+            //3.cek uname dah ada atau belum, kalau udah bisalanjut kalau belum ulang dari masukin uname
+                    if(usernameNew == user[i].username){
+                        found=true;
+                        break;
+                    }
+                }
+            }while(found);
+            // jangan lupa kalau berhasil pindahin dari username new ke database username
+            if(found == false){
+                //4. masukin pass
+                cout<<"\n password :";cin>>passwordNew;
+
+                user[jumlahUser].username = usernameNew;
+                user[jumlahUser].password = passwordNew;
+
+                cout<<"\n selamat registrasi berhasil";
+            }
             
+            //5. simpan di database pass and uname
             break;
+        }
         case 2:
             //Login
+            //1.masukin uname and password, kasih eror handling uname atau password yang salah
+            
+            do{
+                cout<<"\n=========== LOGIN PAGE ===============";
+                cout<<"\n Username : ";cin>>logUser;
+                cout<<"\n Password : ";cin>>logPass;
+                
+                for(int j;j<jumlahUser;j++){
+                    if(user[j].password == logPass && user[j].username == logUser){
+
+                    }
+                }
+
+            }while();
+            //2. if benar masuk applikasi kalau salah balik ke halaman login
+            //a.buat app disini masuk ke fungsi appnya tampilan menu
 
             break;
         case 3:
+            //keluar program
             //exit
             isexit = true;
             break;
