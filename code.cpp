adf616a7966f392d0b337f47b85312d84a67692d
aisyalmas <almasaisyah06@gmail.com>
2026-05-19 00:49:34 +0700

    menu 1 profile

--- code.cpp
+++ code.cpp
@@ -3,16 +3,49 @@ using namespace std;
 
 int isexit = false;
 
+struct userProfile{
+    int currentAge;
+    int targetAge;
+    long double monthlyExpense;
+    double inflationRate;
+    bool isDone;
+    
+};
+
 struct users{
     string username;
     string password;
+    userProfile Profile;
+
 };
 
+int jumlahUser=0,menuAwal,userMax=100;
+int userAktif=-1;
+string usernameNew,passwordNew,logPass,logUser;
+
+void menuProfile(){
+    users u[userAktif];
+
+    //buat pengkondisian kallau pernah ngisi profile belum kalao belum ngisi kaalu udah tampilin yang lama
+    if(u[userAktif].Profile.isDone){
+
+    }
+    //ngisi profile kalau belum
+    cout<<"\n Masukan data Profile Anda: \n";
+    cout<<"\n Umur Sekarang : ";cin>>u[userAktif].Profile.currentAge;
+    cout<<"\n Target Pensiun Dini : ";cin>>u[userAktif].Profile.targetAge;
+    cout<<"\n Asumsi Tingkat Inflasi(%) : ";cin>>u[userAktif].Profile.inflationRate;
+
+    // buat proses penghitungan aturan 4%+inflasi
+    //1. hitung pengeluaran pertahun function
+    //2. proyeksi infalsi masa depan function
+    //3. target asetnya minimal menggunakan the 4%rule kaliin 25 function 
+    //tampilin hasil hitungan
+
+}
 
 
 int main(){
-    int jumlahUser=0,menuAwal,userMax=100;
-    string usernameNew,passwordNew,logPass,logUser;
     users user[userMax];
 
     do{
@@ -60,15 +93,17 @@ int main(){
                 }
             }while(found);
             // jangan lupa kalau berhasil pindahin dari username new ke database username
-            if(found == false){
+            
                 //4. masukin pass
                 cout<<"\n password :";cin>>passwordNew;
 
                 user[jumlahUser].username = usernameNew;
                 user[jumlahUser].password = passwordNew;
+                user[jumlahUser].Profile.isDone=false;
 
+                jumlahUser++;
                 cout<<"\n selamat registrasi berhasil";
-            }
+
             
             //5. simpan di database pass and uname
             break;
@@ -76,25 +111,30 @@ int main(){
         case 2:
             //Login
             //1.masukin uname and password, kasih eror handling uname atau password yang salah
-            
+            bool login=false;
             do{
                 cout<<"\n=========== LOGIN PAGE ===============";
                 cout<<"\n Username : ";cin>>logUser;
                 cout<<"\n Password : ";cin>>logPass;
                 
+                //2. if benar masuk applikasi kalau salah balik ke halaman login
                 for(int j;j<jumlahUser;j++){
                     if(user[j].password == logPass && user[j].username == logUser){
-
+                        login=true;
+                        userAktif=j;
+                        break;
                     }
                 }
 
-            }while();
-            //2. if benar masuk applikasi kalau salah balik ke halaman login
-            //a.buat app disini masuk ke fungsi appnya tampilan menu
+            }while(login);
+            
+            cout<<"\n Selamat Anda berhasil LOGIN";
+            //function buat menu apps
 
             break;
         case 3:
             //keluar program
+            cout<<"terimakasih telah menggunakan apps kammi ;))";
             //exit
             isexit = true;
             break;
@@ -108,3 +148,4 @@ int main(){
     }while (isexit == false);
 
 }
+
